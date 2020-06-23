42fc26ecba2d523833c03a1897291a122c4d6728
ajay-gov <ajay.gov@protonmail.com>
2020-06-23 00:16:49 -0700

    Add setup for running apps in a multi-node cluster by dynamically computing the mesh configurations for each node

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ Execute the command `./run.sh` from a console pointed to the application's home 
 ## View the UI
 Open the following URL on your browser: http://localhost:9002.
 
-## Run as a Fabric
+## Run as a Fabric with two nodes
 
 Run two Swim instances on your local machine to distribute the applications
 Web Agents between the two processes.
@@ -51,3 +51,51 @@ http://localhost:9008 (Server A) or http://localhost:9009 (Server B).  You
 will see a live view of all Web Agents, regardless of which server you point
 your browser at.  Swim transparently demultiplexes links opened by external
 clients, and routes them to the appropriate server in the fabric.
+
+## Run as a Fabric with a multi-node cluster
+Each node in a fabric requires a slightly different configuration. Statically defining the configuration file for each 
+node in a multi-node cluster is quite tedious. Programmatically computing the configurations for each node is
+achievable provided the setup satisfies the following requirements
+- each node in an 'n' node cluster has a unique ordinal index from x through x+n-1.
+- the internal DNS name for a node with index 'x' will be `{$prefix}-x.{$suffix}:{$port}`
+- each node in the cluster is addressable by every other node via a fully qualified DNS name
+
+(These requirements are very similar to a [k8s stateful-set setup](https://kubernetes.io/docs/concepts/workloads/controllers/statefulset/))
+
+The following system properties/environment variables must be defined in each node   
+
+`CLUSTER_PREFIX` - The substring before the index in the DNS name 
+
+`CLUSTER_SUFFIX` - The substring after the index in the DNS name  
+
+`CLUSTER_START` - The start index of the cluster
+
+`CLUSTER_END` - The end index of the cluster
+
+`SWIM_DB_PATH` - The directory path for the swim db files
+
+`HOSTNAME` - The hostname of the specific node. This is unique to each node unlike the previous properties which are the
+same for all the nodes. This is automatically available in some environments.
+
+As an example, let's use k8s as the deployment tool for running this app in a cluster. Since swimos applications are 
+stateful they need to deployed using k8s stateful sets. 
+
+To run a 6 node cluster on port 9002 assuming the following service name and stateful set name:
+```
+Service (ns/name): swimos/transit
+StatefulSet (ns/name): default/transit    
+```
+
+the system properties/environment variables to use will be:
+```
+CLUSTER_PREFIX=transit- 
+CLUSTER_SUFFIX=.transit.swimos.svc.cluster.local:9002 
+CLUSTER_START=0 
+CLUSTER_END=5 
+SWIM_DB_PATH=/tmp/swim-transit # or the mount point of your storage volume
+```
+`HOSTNAME` is an environment variable that is automatically available for each node using k8s stateful sets and it gets 
+evaluated to `transit-{$index}`.
+
+The way to run the app will be to start the `TransitPlane` class in each of the nodes with the appropriate system 
+properties/environment variables.
--- server/src/main/java/swim/transit/TransitPlane.java
+++ server/src/main/java/swim/transit/TransitPlane.java
@@ -17,49 +17,182 @@ package swim.transit;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
-import swim.api.SwimAgent;
-import swim.api.SwimRoute;
-import swim.api.agent.AgentRoute;
+import swim.actor.ActorHostDef;
+import swim.actor.ActorMeshDef;
+import swim.actor.ActorPartDef;
+import swim.actor.ActorSpace;
+import swim.actor.ActorSpaceDef;
+import swim.api.agent.AgentDef;
 import swim.api.plane.AbstractPlane;
 import swim.api.ref.SwimRef;
-import swim.api.space.Space;
+import swim.api.space.SpaceDef;
+import swim.collections.HashTrieMap;
+import swim.db.StoreSettings;
 import swim.kernel.Kernel;
+import swim.kernel.KernelException;
+import swim.kernel.KernelLoader;
+import swim.runtime.HostDef;
+import swim.runtime.NodeDef;
+import swim.runtime.PartDef;
+import swim.runtime.PartPredicate;
 import swim.server.ServerLoader;
-import swim.transit.agent.AgencyAgent;
-import swim.transit.agent.CountryAgent;
-import swim.transit.agent.StateAgent;
-import swim.transit.agent.VehicleAgent;
+import swim.store.StoreDef;
+import swim.store.db.DbStoreDef;
+import swim.structure.Item;
+import swim.structure.Text;
+import swim.structure.Value;
 import swim.transit.model.Agency;
+import swim.uri.Uri;
+import swim.uri.UriMapper;
 
 public class TransitPlane extends AbstractPlane {
-  @SwimAgent("country")
-  @SwimRoute("/country/:id")
-  AgentRoute<CountryAgent> transitAgent;
-
-  @SwimAgent("state")
-  @SwimRoute("/state/:country/:state")
-  AgentRoute<StateAgent> stateAgent;
-
-  @SwimAgent("agency")
-  @SwimRoute("/agency/:country/:state/:id")
-  AgentRoute<AgencyAgent> agencyAgent;
-
-  @SwimAgent("vehicle")
-  @SwimRoute("/vehicle/:country/:state/:agency/:id")
-  AgentRoute<VehicleAgent> vehicleAgent;
 
   public static void main(String[] args) {
-    final Kernel kernel = ServerLoader.loadServer();
-    final Space space = kernel.getSpace("transit");
-
+    final Kernel kernel = loadServer(KernelLoader.class.getClassLoader());
+    final ActorSpace space = (ActorSpace) kernel.getSpace("transit");
     kernel.start();
-    System.out.println("Running TransitPlane...");
+    System.out.println("Running Transit App ....");
+    kernel.run();
 
     startAgencies(space);
+  }
 
-    kernel.run(); // blocks until termination
+  public static Kernel loadServer(ClassLoader classLoader) {
+    try {
+      Value kernelConfig = KernelLoader.loadConfig(classLoader);
+      if (kernelConfig == null) {
+        kernelConfig = KernelLoader.loadConfigResource(classLoader, "server.recon");
+      }
+      if (kernelConfig == null) {
+        kernelConfig = Value.absent();
+      }
+      final Kernel kernel = ServerLoader.loadServerStack(classLoader, kernelConfig);
+      loadSpaces(kernel, kernelConfig);
+      ServerLoader.loadServices(kernel, kernelConfig, classLoader);
+      return kernel;
+    } catch (IOException cause) {
+      throw new KernelException(cause);
+    }
+  }
+
+  private static void loadSpaces(Kernel kernel, Value kernelConfig) {
+    final String clusterPrefix = prop("CLUSTER_PREFIX");
+    final String clusterSuffix = prop("CLUSTER_SUFFIX");
+    final int clusterStart = intProp("CLUSTER_START", -1);
+    final int clusterEnd = intProp("CLUSTER_END", -1);
+    final int self = getSelf(clusterPrefix, -1);
+
+    for (int i = 0, n = kernelConfig.length(); i < n; i += 1) {
+      final Item item = kernelConfig.getItem(i);
+      final SpaceDef spaceDef = kernel.defineSpace(item);
+      if (spaceDef != null) {
+        if (spaceDef instanceof ActorSpaceDef) {
+          ActorSpaceDef actorSpaceDef = (ActorSpaceDef) spaceDef;
+          final HashTrieMap<Value, PartDef> partDefs =
+                  partDefs(actorSpaceDef, self, clusterStart, clusterEnd, clusterPrefix, clusterSuffix);
+          // add the mesh
+          final ActorMeshDef defaultMeshDef = new ActorMeshDef(Uri.empty(), partDefs, UriMapper.empty(), UriMapper.empty(),
+                  UriMapper.empty(), null, null, null, null);
+          actorSpaceDef = actorSpaceDef.meshDef(defaultMeshDef);
+          kernel.openSpace(actorSpaceDef);
+          //System.out.println("Default Mesh Def " + defaultMeshDef);
+        } else {
+          kernel.openSpace(spaceDef);
+        }
+      }
+    }
+  }
+
+  private static HashTrieMap<Value, PartDef> partDefs(ActorSpaceDef spaceDef, int self, int clusterStart, int clusterEnd,
+                                                      String clusterPrefix, String clusterSuffix) {
+    final String dbPath = prop("SWIM_DB_PATH");
+    final int dpPageCacheSize = intProp("SWIM_DB_PAGE_CACHE_SIZE", 4096);
+
+    System.out.println("Self: " + self);
+    HashTrieMap<Value, PartDef> partDefs = HashTrieMap.empty();
+    if (self != -1 && clusterStart != -1 && clusterEnd != -1 && !clusterPrefix.equals("")) {
+      final long partitionEnd = 4294967294L;
+      final long block =  (partitionEnd / (clusterEnd - clusterStart + 1));
+      int index = 0;
+      long lowerBound = 0;
+      long upperBound = lowerBound + block;
+
+      while (index <= (clusterEnd - clusterStart)) {
+        final int key = clusterStart + index;
+
+        for (NodeDef nodeDef : spaceDef.nodeDefs()) {
+          final String hostUri = key == self ? "" : "warp://" + clusterPrefix + key + clusterSuffix;
+          final PartDef partDef = makePartDef(nodeDef, hostUri, key, (int) lowerBound, (int) upperBound,
+                  dbPath, dpPageCacheSize);
+          partDefs = partDefs.updated(partDef.partKey(), partDef);
+        }
+
+        index += 1;
+        lowerBound = upperBound + 1;
+        upperBound = lowerBound + block;
+      }
+    }
+    return partDefs;
+  }
+
+  private static PartDef makePartDef(NodeDef nodeDef, String hostUri, int key, int lowerBound, int upperBound,
+                                     String dbPath, int dpPageCacheSize) {
+    final PartPredicate partPredicate =
+            PartPredicate.and(PartPredicate.node(nodeDef.nodePattern()), PartPredicate.hash(lowerBound, upperBound));
+    final HostDef hostDef = ActorHostDef.fromHostUri(hostUri).isPrimary(true);
+    final String nodeKey = nodeKey(nodeDef);
+    final ActorPartDef actorPartDef = ActorPartDef.fromPartPredicate(Text.from(nodeKey + key), partPredicate)
+            .hostDef(hostDef);
+    if (dbPath == null || dbPath.trim().equals("")) {
+      return actorPartDef;
+    } else {
+      final String storePath = dbPath + "/" + nodeKey;
+      StoreDef storeDef = new DbStoreDef("", storePath, StoreSettings.standard().pageCacheSize(dpPageCacheSize));
+      return actorPartDef.storeDef(storeDef);
+    }
+  }
+
+  private static String nodeKey(NodeDef nodeDef) {
+    final Collection<? extends AgentDef> agentDefs = nodeDef.agentDefs();
+    final String def = nodeDef.nodePattern().toString();
+    if (!agentDefs.isEmpty()) {
+      return agentDefs.iterator().next().id().stringValue(def);
+    } else {
+      return def;
+    }
+  }
+
+  private static int getSelf(String clusterPrefix, int def) {
+    final String s = prop("HOSTNAME").trim();
+    if (!clusterPrefix.equals("") && s.startsWith(clusterPrefix)) {
+      final String s1 = s.substring(clusterPrefix.length());
+      try {
+        return Integer.parseInt(s1);
+      } catch (NumberFormatException e) {
+      }
+    }
+    return def;
+  }
+
+  private static String prop(String prop) {
+    String s = System.getProperty(prop);
+    if (s == null) {
+      s = System.getenv(prop);
+    }
+    System.out.println("Property " + prop + "=" + s);
+    return s == null ? "" : s;
+  }
+
+  private static int intProp(String prop, int def) {
+    try {
+      final String s = prop(prop);
+      return Integer.parseInt(s);
+    } catch (NumberFormatException e) {
+      return def;
+    }
   }
 
   private static void startAgencies(SwimRef swim) {
